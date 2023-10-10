02dcc84b48d7661420fdadc671f009df9cd2026e
RenanCQueiroz <50963337+RenanCQueiroz@users.noreply.github.com>
2023-10-09 21:23:46 -0400

    Added the whole frontend
    Bare bones templates from Skeleton (pun intended) + some minor additions i did

---
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	
+</script>
+
+<slot />
---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">MEu app é top dms !!!!! </h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+</div>
+
+<script>
+	import { Avatar, ProgressBar } from '@skeletonlabs/skeleton';
+</script>
+
+
+<ProgressBar label="Progress Bar" value={50} max={100} />
